6b727efcd26fff24e1dda83a7fbe1c35cefc0e27
lnab <2385235556@qq.com>
2025-05-27 01:41:54 +0800

    我再试试push

--- source/lab.cpp
+++ source/lab.cpp
@@ -1102,7 +1102,7 @@ void lab::Reserve::Logic(ui::Screen *screen) noexcept
         SwitchTo(new lab::EnterReserve);
     });
 }
-//刚才push不上去，写个注释
+
 void lab::Reserve::Ready(ui::Screen *screen) noexcept
 {
     ;
